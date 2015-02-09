b2c12bc0e0f9a01e3787f5054d212fff19041ac6
Jarah <jarah@wom-marketing.nl>
2015-02-09 23:46:34 +0100

    Mogelijkheid bon aanmaken

--- app/controllers/BonnenController.php
+++ app/controllers/BonnenController.php
@@ -5,9 +5,9 @@ class BonnenController extends BaseController {
 	/* Get functions */
 	public function getAdminList()
 	{
-		$machines = Machine::all();
+		$bonnen = Bon::groupBy('bon_id')->get();
 		
-		return View::make('admin.machines.list')->with('machines', $machines);
+		return View::make('admin.bonnen.list')->with('bonnen', $bonnen);
 	}
 	
 	public function getAdminEdit($machine_id)
@@ -17,11 +17,14 @@ class BonnenController extends BaseController {
 		return View::make('admin.machines.edit')->with('machine', $machine);
 	}
 	
-	public function getAdminView($machine_id)
+	public function getAdminView($bon_id=null,$klant_id=null)
 	{
-		$machine = Machine::find($machine_id);
-		$standen = Stand::where('m_id', '=', $machine->id)->get();
-		return View::make('admin.machines.view')->with('machine', $machine)->with('standen', $standen);
+		$bon = Bon::where('bon_id','=',$bon_id)->get();
+		$klant = Klant::where('id','=',$klant_id)->firstOrFail();
+		
+       return View::make('admin.bonnen.view')
+       		->with('klant', $klant)
+			->with('bon', $bon);
 	}
 	
 	public function getNew($klant_id) {
@@ -36,14 +39,58 @@ class BonnenController extends BaseController {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
 	/* Do functions */
+	public function doSaveTemp($klant_id) {
+		$machines = Machine::where('klant_id', '=', $klant_id)->get();
+		
+		if(Input::get('calculate'))
+		{
+			Session::put('calculate', true );
+			
+			foreach( $machines as $machine ){
+				
+				$nieuw_in=Input::get('nieuw_in'.$machine->id);
+				
+				$nieuw_uit=Input::get('nieuw_uit'.$machine->id);
+				
+				$tikken_uit=Input::get('tikken_uit'.$machine->id);
+				
+				$stand = Stand::where('m_id', '=', $machine->id)->firstOrFail();
+				
+				Session::put('nieuw_in'.$machine->id, $nieuw_in );
+				Session::put('nieuw_uit'.$machine->id, $nieuw_uit );
+				Session::put('tikken_uit'.$machine->id, $tikken_uit );
+				
+				Session::put('in'.$machine->id, ( ($nieuw_in-$stand->e_stand) * 0.1));
+				Session::put('uit'.$machine->id, ( ($nieuw_uit-$tikken_uit) * 0.1));
+			}
+		}
+		
+		if(Input::get('save')){
+			
+			foreach( $machines as $machine ){
+				
+				$bon = new Bon();
+	            $bon->klant_id = $klant_id;
+	            $bon->bon_id = 2;
+				$bon->machine_id = $machine->id;	
+				$bon->nieuw_in 		= 	Session::get('nieuw_in'.$machine->id);
+				$bon->nieuw_uit 	= 	Session::get('nieuw_uit'.$machine->id );
+				$bon->tikken_uit 	= 	Session::get('tikken_uit'.$machine->id);
+				$bon->in1 			= 	Session::get('in'.$machine->id);
+				$bon->uit 		= 	1605; //Session::get('uit'.$machine->id) produces error
+				$bon->created_at = date('Y-m-d H:i:s');
+            	$bon->save();
+			}
+        	return Redirect::back();    
+		}
+		
+		return Redirect::back();
+	}
+	
+	
+	
+	
 	public function doNew() {
         $rules = array(
             'machine_type'   => 'required',
